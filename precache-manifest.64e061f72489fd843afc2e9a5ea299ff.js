96dd28948e09e55c214cb6c313c4d9ed144c075c
xackus <14938807+xackus@users.noreply.github.com>
2020-05-16 00:47:13 +0200

    Updates

---
+++ precache-manifest.64e061f72489fd843afc2e9a5ea299ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab459c5b3b5444d7bfb1d9b99ff06b42",
+    "url": "/hexagon-game/index.html"
+  },
+  {
+    "revision": "70068fa634f8fbb21c1f",
+    "url": "/hexagon-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db0191c6384edc047e99",
+    "url": "/hexagon-game/static/js/2.7e699fb5.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/hexagon-game/static/js/2.7e699fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70068fa634f8fbb21c1f",
+    "url": "/hexagon-game/static/js/main.6e0c603c.chunk.js"
+  },
+  {
+    "revision": "85bee769cfee7a9c96c9",
+    "url": "/hexagon-game/static/js/runtime-main.8d31e29c.js"
+  }
+]);
